c6c8baa4b602c56a6a5ba534bac47a58900187f5
SebastianDCoder <sebastiandurazzini@gmail.com>
2026-04-13 20:06:56 -0300

    Funcionando con exito

--- TP-2-Programacion-3/Ejercicios/Ejercicio3.aspx.designer.cs
+++ TP-2-Programacion-3/Ejercicios/Ejercicio3.aspx.designer.cs
@@ -148,5 +148,14 @@ namespace TP_2_Programacion_3
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LinkButton LinkButton5;
+
+        /// <summary>
+        /// Control ButtonReset.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonReset;
     }
 }
